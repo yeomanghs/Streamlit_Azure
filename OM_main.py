23297cc679816ff1462ffb877f339ffee4a001ed
yeomanghs <30331454+yeomanghs@users.noreply.github.com>
2024-08-19 19:12:48 +0800

    Update OM_main.py
    Comment sklearn.pipeline

--- OM_main.py
+++ OM_main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from sklearn.pipeline import Pipeline
+#from sklearn.pipeline import Pipeline
 from King.Preprocessing import King_Preprocessing
 # from EMEA_OM_King_process import callWebService
 import pandas as pd
