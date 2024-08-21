a78cb415c8d35b8c45c24e287d4dc4530d30fe0e
yeomanghs <30331454+yeomanghs@users.noreply.github.com>
2024-08-21 19:00:51 +1000

    Add files via upload

---
+++ Birmingham/Config.py
@@ -0,0 +1,20 @@
+    
+#predefine parameters
+# colList = ['Region', 'Nationality', 'area_of_study', 'EnrolmentPeriod']\
+#             + ['Age', 'Recency', 'OfferMonth', 'CountryOfResidence', 'OfferYear', 'CourseType', 'IsRisk1', 'StudentType']
+
+#colList needed from raw table i.e keys of _samples.json
+colListNeeded = ['LoS', 'CORSubRegion', 'FirstStudentStatus', 'EnrolledNumeric', 'CourseGroup', 'InitialOfferMonth', 'InitialOfferDay', 'TagValueGroup']
+
+#REST endpoint
+url = 'http://4dcbaca7-2d17-44a8-ad52-41a1de98a525.australiaeast.azurecontainer.io/score'
+
+# #load encoder, trained model
+# encoderFilename = 'C:/Users/JiunShyanGoh/Documents/MachineLearning/Model/UTS/2021-08-06_UTS_Encoder12Features.sav'
+# modelFilename = 'C:/Users/JiunShyanGoh/Documents/MachineLearning/Model/UTS/2021-08-06_UTS_RF_12Features.sav'
+# #file to update area_of_study
+# updateFilename = "C:/Users/JiunShyanGoh/Documents/EDA/Data/Raw/UTS/UTS College Updated Course Info.xlsx"
+# #file for tag history
+# tagFilename = "C:/Users/JiunShyanGoh/Documents/EDA/Data/Raw/UTS/2021-07-28_StudentTagHistory_Insearch.csv"
+
+# threshold = 0.3
---
+++ Birmingham/Preprocessing.py
@@ -0,0 +1,214 @@
+from sklearn.base import BaseEstimator
+import numpy as np
+import pandas as pd
+import re
+from Birmingham.Config import *
+import urllib.request
+import json
+from sklearn import preprocessing
+from sklearn.cluster import KMeans
+
+class Birmingham_Preprocessing(BaseEstimator):
+    def __init__(self, df = None):
+        if isinstance(df, pd.DataFrame):
+            self.df = df
+        
+    def fit(self):
+        return self
+
+    def transform(self, df = None):
+        if isinstance(df, pd.DataFrame):
+            self.df = df
+        scaler = preprocessing.MinMaxScaler()
+        gmailList = ['gmai', 'gmaii', 'gmail', 'gamil', 'gmial', 'gmaill', 'gamail', 'googlemail']
+        hotmailList = ['homail', 'hotmail', 'live', 'outlook']
+        yahooList = ['yahoo', 'yhaoo']
+        groupList = ['gmail', 'hotmail', 'yahoo']
+
+        def getDomainGroup(emailDomainArray):
+            resultList = []
+            
+            for emailDomain in emailDomainArray:
+                domain = ''
+                for no, regexList in enumerate([gmailList, hotmailList, yahooList]):
+                    if re.search("|".join(regexList), str(emailDomain)):
+                        domain = groupList[no]
+                if domain == '':
+                    domain = 'others'
+                resultList.append(domain) 
+            return resultList 
+
+        def getCourseGroup(courseArray):
+            resultList = []
+            for value in courseArray:
+                if re.search('Engineering', str(value), re.I):
+                    group = 'Engineering'
+                elif re.search('Law', str(value), re.I):
+                    group = 'Law'
+                elif re.search('Mathematics', str(value), re.I):  
+                    group = 'Mathematics'
+                elif re.search('Computer Science|Artifical Intelligence', str(value), re.I):  
+                    group = 'IT' 
+                elif re.search('English|Modern Languages|Linguistics|Creative Writing|Translation Studies|French|German|Spanish', str(value), re.I):  
+                    group = 'English/Language'  
+                elif re.search('Economics', str(value), re.I):  
+                    group = 'Economics'  
+                elif re.search('Geology|Science|Biology|Physics|Biotechnology|Biochemistry|Chemistry|Geography', str(value), re.I):  
+                    group = 'Science'  
+                elif re.search('Shakespeare|Philosophy|History|Art|Literature and Culture', str(value), re.I):  
+                    group = 'Literature/Philosophy' 
+                elif re.search('Psychology', str(value), re.I):  
+                    group = 'Psychology'  
+                elif re.search('Education|Teaching', str(value), re.I):  
+                    group = 'Education'
+                elif re.search('Clinical|Public Health|Mental Health|Health Care|Medicine|Immunology|Pharmacy|Health', str(value), re.I):  
+                    group = 'Med/Health'  
+                elif re.search('Business|Finance|Financial Management', str(value), re.I):  
+                    group = 'Business/Finance'  
+                else:
+                    group = 'Others'
+                resultList.append(group)
+            return resultList
+
+        df['CourseGroup'] = getCourseGroup(df['CourseName'].values)
+            
+        #date-related feature
+        df['InitialOfferMonth'] = pd.to_datetime(df['InitialOfferDate'], format='%d/%m/%Y').dt.month.astype(str)
+            
+        dayDict = {0:"Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4:"Friday", 5:"Saturday", 6:"Sunday"}
+
+        df['InitialOfferDay'] = pd.to_datetime(df['InitialOfferDate'], format='%d/%m/%Y').dt.weekday.map(dayDict)
+
+        df.rename(columns = {"tag_value":"Tagvalue"}, inplace = True)
+
+        #harmonize tag_value
+        def getTagValueGroup(tagValueArray):
+            resultList = []
+            for value in tagValueArray:
+                if re.search('^IDP', str(value)):
+                    group = 'IDP-related'
+                elif re.search('^MO UOB.*CO', str(value)):
+                    group = 'MO UOB-related and CO'
+                elif re.search('^MO UOB.*UO', str(value)):  
+                    group = 'MO UOB-related and UO'
+                else:
+                    group = 'Others'
+                resultList.append(group)
+            return resultList
+
+        df['TagValueGroup'] = getTagValueGroup(df['Tagvalue'].values)
+
+        #paste from _sample.json[0].keys()
+        sample_records = df[[i for i in colListNeeded]].to_dict('records')
+        return sample_records
+
+    #consume REST endpoint from Azure AI
+    def callWebService(self, url, body, headers):
+        req = urllib.request.Request(url, body, headers) 
+
+        try:
+            response = urllib.request.urlopen(req)
+
+            # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
+            # req = urllib.request.Request(url, body, headers) 
+            # response = urllib.request.urlopen(req)
+
+            result = response.read()
+            return result
+        #     print(result) 
+        except  urllib.error.HTTPError as error:
+            print("The request failed with status code: " + str(error.code))
+            # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+            print(error.info())
+            print(json.loads(error.read()))  
+
+    def finalProcessing(self, body):
+        headers = {'Content-Type':'application/json'}
+        body = str.encode(json.dumps(body))
+        result = self.callWebService(url = url, body = body, headers = headers)
+        resultJson = json.loads(json.loads(result))
+        dfResult = pd.DataFrame(resultJson['result'], columns = colListNeeded + ['Scored Labels', 'Scored Probabilities'])
+        #Data Manipulation to get final output
+        #predefine parameters
+        threshold = 0.2
+        highTag = "PT_ENP_TagAXXXX_HIGHXXXX"
+        mediumTag = "PT_ENP_TagAXXXX_MEDXXXXX"
+        lowTag = "PT_ENP_TagAXXXX_LOWXXXXX"
+        #version 1
+        tagDate = "19/09/2023"
+        taskType = 'Made Offer'
+        df = self.df.copy()
+        for col in ['Tagvalue', 'IntakeStatus', 'InitialOfferStatus', 'Year', 'COR', 'Campus']:
+            dfResult[col] = df[col]
+        dfResult['Scored Labels'] = np.where(dfResult['Scored Probabilities'].values> threshold, 1, 0)
+        dfResult['General_Tag'] = np.where(dfResult['Scored Labels'] == 1, highTag, lowTag)
+
+        #train and use kmeans
+        xCol = 'Scored Probabilities'
+        dfTemp = dfResult[dfResult['General_Tag'] == lowTag].copy()
+        if dfTemp.shape[0]!=0 and dfTemp.shape[0]!=1:
+            num_clusters = 2
+            kmeans = KMeans(n_clusters = num_clusters)
+            kmeans.fit(dfTemp[[xCol]])
+
+            #assign label
+            dfTemp['General_Tag'] = kmeans.labels_
+            firstCentroid = kmeans.cluster_centers_[0][0]
+            secondCentroid = kmeans.cluster_centers_[1][0]
+            if firstCentroid > secondCentroid:
+                firstCentroidLabel = mediumTag
+                secondCentroidLabel = lowTag
+            else:
+                firstCentroidLabel = lowTag
+                secondCentroidLabel = mediumTag
+            centroidDict = {0: firstCentroidLabel, 1: secondCentroidLabel}
+            dfTemp['General_Tag'] = dfTemp['General_Tag'].map(centroidDict)
+
+            #concatenate high, med and low 
+            dfHigh = dfResult[dfResult['General_Tag'] != lowTag].copy()
+            dfResultFinal = pd.concat([dfHigh, dfTemp]).copy()
+        else:
+            dfResultFinal = dfResult.copy()
+
+        #get back original age       
+        dfResultFinal['tag_date'] = tagDate
+        dfResultFinal['TaskType'] = taskType
+        dfResultFinal['TaskCreate'] = np.where(dfResultFinal['Tagvalue'] == 'NULL', "No", "Yes")
+
+        col         = 'General_Tag'
+        conditions  = [ dfResultFinal[col] == highTag, dfResultFinal[col] == mediumTag, dfResultFinal[col] == lowTag]
+        choices     = [ "HIGH", 'MED', 'LOW' ]
+        dfResultFinal['Assigned To'] = np.select(conditions, choices, default=np.nan)
+        LoS_Dict = {"Postgraduate":"PG", "Undergraduate":"UG", "Foundation":"F"}
+
+        dfResultFinal['IntakeStatus'] = df['IntakeStatus']
+        dfResultFinal['IntakeStatus'] = dfResultFinal['IntakeStatus'].fillna('')
+        dfResultFinal['LoS_ShortForm'] = dfResultFinal['LoS'].map(lambda x:LoS_Dict[x] if x in LoS_Dict.keys() else x)
+        dfResultFinal['InitialOfferStatus'] = dfResultFinal['InitialOfferStatus'].map(lambda x:re.sub("Made Offer", "MO", x))
+        dfResultFinal['Year'] = dfResultFinal['Year'].astype(str).str[-2:]
+        dfResultFinal['COR'] = np.where(pd.isnull(dfResultFinal['COR']), '', dfResultFinal['COR'])
+        dfResultFinal['LoS'] = dfResultFinal['LoS'].map(lambda x:{"Postgraduate":"PG", "Undergraduate":"UG"}.get(x, x))
+        dfResultFinal['Task_Description'] = dfResultFinal['IntakeStatus'].astype(str) + '-' + dfResultFinal['Year'] + '_'  +  dfResultFinal['InitialOfferStatus'] \
+                                + ' ' + dfResultFinal['COR']  + ' ' + dfResultFinal['LoS_ShortForm']
+
+        #taskCreate = No and task_description empty for COR!=China
+        dfResultFinal['TaskCreate'] = dfResultFinal['COR'].map(lambda x: 'Yes' if x == 'China' else 'No')
+        dfResultFinal['Task_Description'] = dfResultFinal.apply(lambda x:x['Task_Description'] if x['COR'] == 'China' else '',
+            axis = 1)
+
+        finalColumnList = ["AA ClientName", "AA client_id", "AB crms_number","AC_MOTask", "AE_MLTag", 
+               "Age", "AgentDirect", "AreaOfStudy", "Campus", "ClientType", "COR", 
+               "CORRegion", "CORSubRegion", "CourseName", "Dead", "EmailDomain", 
+               "EnrolledNumeric", "EnrPeriod", "Faculty", "FeeStatus", "FirstAgentTag", 
+               "FirstStudentStatus", "Gender", "InclusionStatus", "InitialOfferStatus", "IntakeStatus",
+               "LoS", "Nationality", "NatRegion", "NatSubRegion", "OfferTagSeq",
+               "ProspectType", "Tagvalue", "Year", "Zone", "Number of Records",
+               "InitialOfferDate","CensusDate"
+               ] + ['Scored Labels', 'Scored Probabilities', 'tag_date', 'TaskType',
+                   'TaskCreate', 'General_Tag', 'Assigned To', 'Task_Description']
+        for col in finalColumnList:
+            if col not in dfResultFinal.columns:
+                if col in df.columns:
+                    dfResultFinal[col] = df[col]
+        dfFinal = dfResultFinal[finalColumnList].copy()
+        return dfFinal
